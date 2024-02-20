c7db510c18250205616c2c4a9a848e1b0e5947c0
julioceaseless <julioceaseless@gmail.com>
2024-02-20 12:16:17 +0000

    sql setup

---
+++ setup_mysql_dev.sql
@@ -0,0 +1,13 @@
+-- Create database
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+
+-- Create the user if it does not exist
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+
+-- Grant all privileges on the hbnb_dev_db database to the hbnb_dev user
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
+
+-- Grant SELECT privilege on the performance_schema database to the hbnb_dev user
+GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
+
+SHOW DATABASES;
