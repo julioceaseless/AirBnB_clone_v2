196b1303a4f3a1dfa41e648f7479214e9fba6422
julioceaseless <julioceaseless@gmail.com>
2024-02-21 09:36:22 +0000

    added a delete method to delete an object from objects

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -25,6 +25,14 @@ class FileStorage:
                 temp[key] = val.to_dict()
             json.dump(temp, f)
 
+    def delete(self, obj=None):
+        """delete object from storage"""
+        if obj:
+            """create key for the obj"""
+            key = f'{obj.__class__.__name__}.{obj.id}'
+            """delete from __objects dictionary"""
+            del FileStorage.__objects[key]
+
     def reload(self):
         """Loads storage dictionary from file"""
         from models.base_model import BaseModel
@@ -45,6 +53,6 @@ class FileStorage:
             with open(FileStorage.__file_path, 'r') as f:
                 temp = json.load(f)
                 for key, val in temp.items():
-                        self.all()[key] = classes[val['__class__']](**val)
+                    self.all()[key] = classes[val['__class__']](**val)
         except FileNotFoundError:
             pass
