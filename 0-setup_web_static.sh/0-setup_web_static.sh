8a9e92323ade681c408729b9202a13a28a2d8b13
julioceaseless <julioceaseless@gmail.com>
2024-03-07 05:25:59 +0000

    configure airbnb project file system

---
+++ 0-setup_web_static.sh/0-setup_web_static.sh
@@ -0,0 +1,48 @@
+#!/usr/bin/env bash
+# script sets up web servers for deployment of web_static
+
+# update packages
+sudo apt-get -y update
+
+# install nginx if not already installed
+sudo apt-get install -y nginx
+
+# create folder if it does not exist
+sudo mkdir -p /data/web_static/{shared,releases}/
+
+shared="/data/web_static/shared"
+releases="/data/web_static/releases"
+
+# create test folder
+sudo mkdir $releases/test/
+
+placeholder="
+<html>
+  <head>
+  </head>
+  <body>
+    Holberton School
+  </body>
+</html>"
+	
+# create test index.html
+sudo echo "$placeholder" > $releases/test/index.html
+
+# create a symbolic link to test folder forcefully
+sudo ln -s -f $releases/test/ /data/web_static/current
+
+# give ownership of /data/ folder to user 'ubuntu' and group 'ubuntu'
+sudo chown -R ubuntu:ubuntu /data/
+
+# set up alias
+hbnb_static="\ \n \tlocation /hbnb_static {\n
+                \t\talias /data/web_static/current/;\n
+                \t\tautoindex off;\n
+        \t}\n"
+
+# append new alias
+sudo sed -i "/^[[:space:]]}/ a $(echo $hbnb_static)"\
+	/etc/nginx/sites-{enabled,available}/default 
+
+# restart nginx to apply configuration changes
+sudo service nginx restart
