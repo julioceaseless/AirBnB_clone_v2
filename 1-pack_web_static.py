715eb9c077185203b68f8b1db50a1f5d242216cc
julioceaseless <julioceaseless@gmail.com>
2024-03-08 02:23:19 +0000

    create a tarball locally

---
+++ 1-pack_web_static.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+""" script generates a .tgz archive from the contents of webstatic
+folder for AirBnB Clone"""
+from fabric.api import local
+from datetime import datetime
+
+
+def do_pack():
+    """ Create a .tgz archive from the contents of webstatic folder"""
+
+    # get time now
+    now = datetime.now().strftime("%Y%m%d%H%M%S")
+
+    # create a archive path
+    archive_path = f"versions/web_static_{now}.tgz"
+
+    # create directory to store all the archive files
+    local("mkdir -p versions")
+
+    # create an archive
+    tar_ball = local(f"tar -cvzf {archive_path} web_static")
+
+    # return the archive path if successfully or none
+    return archive_path
+
+
+if __name__ == "__main__":
+    """run function only when called directly"""
+    do_pack()
