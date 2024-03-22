67a2907c7df53d837b3cf205fb6da20840b3da53
julioceaseless <julioceaseless@gmail.com>
2024-03-22 15:36:05 +0000

    add /hbnb route

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+""" script starts a Flask web app """
+from flask import Flask
+
+
+# initialize flask
+app = Flask(__name__)
+
+# define route for app's home dir
+@app.route("/", strict_slashes=False)
+def hbnb():
+    """ home"""
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb_dir():
+    """ hbnb dir """
+    return "HBNB"
+
+
+if __name__ == "__main__":
+    """ run only when called directly"""
+    app.run(host="0.0.0.0", port=5000)
