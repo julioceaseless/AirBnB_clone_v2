f880a49b7280003ad864cf2c5e0e008400065b2a
julioceaseless <julioceaseless@gmail.com>
2024-04-18 12:52:53 +0000

    fix foreign key problem

--- models/place.py
+++ models/place.py
@@ -32,8 +32,8 @@ class Place(BaseModel, Base):
     """
     __tablename__ = 'places'
 
-    city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
     user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
+    city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
     name = Column(String(128), nullable=False)
     description = Column(String(1024), nullable=True)
     number_rooms = Column(Integer, nullable=False, default=0)
