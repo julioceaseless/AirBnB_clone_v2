e7d956e54c9ae65fe6f0eccdf61b3f6b4a15dd7d
julioceaseless <julioceaseless@gmail.com>
2024-04-18 10:21:18 +0000

    changes to support sqlalchemy

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,6 +1,19 @@
 #!/usr/bin/python3
 """This module defines a class to manage file storage for hbnb clone"""
 import json
+from models.base_model import BaseModel
+from models.user import User
+from models.place import Place
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.review import Review
+
+classes = {
+        'BaseModel': BaseModel, 'User': User, 'Place': Place,
+        'State': State, 'City': City, 'Amenity': Amenity,
+        'Review': Review
+        }
 
 
 class FileStorage:
@@ -27,24 +40,13 @@ class FileStorage:
 
     def reload(self):
         """Loads storage dictionary from file"""
-        from models.base_model import BaseModel
-        from models.user import User
-        from models.place import Place
-        from models.state import State
-        from models.city import City
-        from models.amenity import Amenity
-        from models.review import Review
 
-        classes = {
-                    'BaseModel': BaseModel, 'User': User, 'Place': Place,
-                    'State': State, 'City': City, 'Amenity': Amenity,
-                    'Review': Review
-                  }
         try:
             temp = {}
-            with open(FileStorage.__file_path, 'r') as f:
+            with open(FileStorage.__file_path, "r", encoding="UTF-8") as f:
                 temp = json.load(f)
-                for key, val in temp.items():
-                        self.all()[key] = classes[val['__class__']](**val)
+                for key, dict_value in temp.items():
+                    dict_value = classes[dict_value["__class__"]](**dict_value)
+                    self.__objects[key] = dict_value
         except FileNotFoundError:
             pass
--- models/state.py
+++ models/state.py
@@ -1,8 +1,33 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+from os import environ
 
 
-class State(BaseModel):
+class State(BaseModel, Base):
     """ State class """
-    name = ""
+    __tablename__ = "states"
+    name = Column(String(128), nullable=False)
+
+    if environ['HBNB_TYPE_STORAGE'] == 'db':
+        cities = relationship(
+                'City', cascade='all, delete, delete-orphan',
+                backref='state')
+    else:
+        @property
+        def cities(self):
+            """
+            getter method for cities
+            returns the list of Cities where state_id equals
+            self.id
+            """
+            from models import storage
+            from models.city import City
+            related_cities = []
+            cities_dict = storage.all(City)
+            for city in cities_dict.values():
+                if city.state_id == self.id:
+                    related_cities.append(city)
+            return related_cities
